f0caccb1e01c44f99df98a185118f2e3d72fbbdf
Peter Somers <psomers3@gatech.edu>
2020-02-17 10:34:13 +0100

    initial commit

---
+++ Arduino-i2c-pwm-encoders/Arduino_i2c_pwm_encoders.ino
@@ -0,0 +1,67 @@
+//
+// Arduino-i2c-pwm-encoders
+//
+// Turn Arduino into PWM Driver and Quadrature Decoder with I2C communication
+// Developed with [embedXcode](https://embedXcode.weebly.com)
+//
+// Author 		Peter Somers
+// 				Peter Somers
+//
+// Date			2/17/20 10:12 AM
+// Version		<#version#>
+//
+// Copyright	© Peter Somers, 2020
+// Licence		<#licence#>
+//
+// See         ReadMe.txt for references
+//
+
+
+// Core library for code-sense - IDE-based
+// !!! Help: http://bit.ly/2AdU7cu
+#if defined(ENERGIA) // LaunchPad specific
+#include "Energia.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#include "Arduino.h"
+#elif defined(ESP8266) // ESP8266 specific
+#include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.8 specific
+#include "Arduino.h"
+#else // error
+#error Platform not defined
+#endif // end IDE
+
+// Set parameters
+
+
+// Include application, user and local libraries
+// !!! Help http://bit.ly/2CL22Qp
+
+
+// Define structures and classes
+
+
+// Define variables and constants
+
+
+// Prototypes
+// !!! Help: http://bit.ly/2l0ZhTa
+
+
+// Utilities
+
+
+// Functions
+
+
+// Add setup code
+void setup()
+{
+    ;
+}
+
+// Add loop code
+void loop()
+{
+    ;
+}
