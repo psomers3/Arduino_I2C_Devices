4623647cc41620d7f98853bc4dbabb734cc3b99d
ISYS <st164174@stud.uni-stuttgart.de>
2020-02-27 13:00:17 +0100

    switching to smbus

--- ArduinoI2CDevices_Python/Modules/ArduinoServo.py
+++ ArduinoI2CDevices_Python/Modules/ArduinoServo.py
@@ -1,4 +1,4 @@
-import pylibi2c
+from smbus2 import SMBus, i2c_msg
 import struct
 
 commands = {
@@ -8,10 +8,12 @@ commands = {
     'DETACH': 0x03
 }
 
+FWD_TO_DEV = 0x03
+
 
 class ArduinoServo(object):
     def __init__(self,
-                 i2c_dev: pylibi2c.I2CDevice,
+                 i2c_bus: SMBus,
                  arduino_addr,
                  dev_id,
                  min_period=1000,
@@ -19,41 +21,28 @@ class ArduinoServo(object):
         self.min_period = min_period
         self.max_period = max_period
         self.range = self.max_period - self.min_period
-        self.i2c_dev = i2c_dev
+        self.i2c_bus = i2c_bus
         self.arduino_addr = arduino_addr
-        self.dev_id = (dev_id << 4) | 4
+        self.dev_id = dev_id
 
     def write(self, angle: float):  # from 0-180
         self.write_percentage(angle/180)
-        # as_int = int(angle*100)
-        # short = struct.pack('H', as_int)
-        # message = struct.pack('bb', self.dev_id, commands['WRITE'])
-        # new_message = bytearray(4)
-        # new_message[0:1] = message
-        # new_message[2:] = short
-        # final_message = bytes(new_message)
-        # message = int.from_bytes(final_message, byteorder='big')  # convert to number for i2c library
-        # self.i2c_dev.read(message, 1)
 
     def write_micro_seconds(self, micro_seconds: int):
         short = struct.pack('H', micro_seconds)
-        message = struct.pack('bb', self.dev_id, commands['WRITE_MS'])
-        new_message = bytearray(4)
-        new_message[0:1] = message
-        new_message[2:] = short
-        final_message = bytes(new_message)
-        message = int.from_bytes(final_message, byteorder='big')  # convert to number for i2c library
-        self.i2c_dev.read(message, 1)
+        write_msg = i2c_msg.write(self.arduino_addr, [FWD_TO_DEV, self.dev_id, commands['WRITE_MS'], short[0], short[1]])
+        read_msg = i2c_msg.read(self.arduino_addr, 1)
+        self.i2c_bus.i2c_rdwr(write_msg, read_msg)
 
     def write_percentage(self, percent: float):  # 0-100
         self.write_micro_seconds(int(self.range*percent + self.min_period))
 
     def attach(self):
-        message = struct.pack('bbbb', self.dev_id, commands['ATTACH'], 0x00, 0x00)
-        message = int.from_bytes(message, byteorder='big')  # convert to number for i2c library
-        self.i2c_dev.read(message, 1)
+        write_msg = i2c_msg.write(self.arduino_addr, [FWD_TO_DEV, self.dev_id, commands['ATTACH']])
+        read_msg = i2c_msg.read(self.arduino_addr, 1)
+        self.i2c_bus.i2c_rdwr(write_msg, read_msg)
 
     def detach(self):
-        message = struct.pack('bbbb', self.dev_id, commands['DETACH'], 0x00, 0x00)
-        message = int.from_bytes(message, byteorder='big')  # convert to number for i2c library
-        self.i2c_dev.read(message, 1)
+        write_msg = i2c_msg.write(self.arduino_addr, [FWD_TO_DEV, self.dev_id, commands['DETACH']])
+        read_msg = i2c_msg.read(self.arduino_addr, 1)
+        self.i2c_bus.i2c_rdwr(write_msg, read_msg)
