b965174cf14acd63b61ddc15f6ae04035b5c2409
ISYS <st164174@stud.uni-stuttgart.de>
2020-03-01 13:06:31 +0100

    updated set_encoder_update_freq

--- ArduinoI2CDevices_Python/Modules/ArduinoI2CBus.py
+++ ArduinoI2CDevices_Python/Modules/ArduinoI2CBus.py
@@ -34,9 +34,7 @@ class ArduinoI2CBus(object):
         write_msg = smbus2.i2c_msg.write(self.arduino_addr, [first_byte_cmd['NEW_ENCODER_DEVICE'], pin_A, pin_B])
         read_msg = smbus2.i2c_msg.read(self.arduino_addr, 1)
         self.i2c_bus.i2c_rdwr(write_msg, read_msg)
-        device_id = ord(read_msg.buf[0])
-        print('encoder dev', device_id)
-        new_encoder = ArduinoEncoder(self.i2c_bus, self.arduino_addr, device_id)
+        new_encoder = ArduinoEncoder(self.i2c_bus, self.arduino_addr, ord(read_msg.buf[0]))
         self.devices.append(new_encoder)
         return new_encoder  # type: ArduinoEncoder
 
@@ -45,15 +43,10 @@ class ArduinoI2CBus(object):
         Will change update rate for all encoder devices on the arduino
         :param freq: global update frequency in Hz
         """
-        short = struct.pack('H', freq)
-        message = struct.pack('b', first_byte_cmd['SET_ENCODER_UPDATE_FREQ'])
-        new_message = bytearray(4)
-        new_message[0] = message
-        new_message[1:2] = short
-        new_message[-1] = bytes(0x00)
-        final_message = bytes(new_message)
-        message = int.from_bytes(final_message, byteorder='big')  # convert to number for i2c library
-        response = self.i2c_bus.read_byte_data(self.arduino_addr, message)
+        short = struct.unpack('2B', struct.pack('H', freq))
+        write_msg = smbus2.i2c_msg.write(self.arduino_addr, [first_byte_cmd['SET_ENCODER_UPDATE_FREQ'], short[0], short[1]])
+        read_msg = smbus2.i2c_msg.read(self.arduino_addr, 1)
+        self.i2c_bus.i2c_rdwr(write_msg, read_msg)
 
     def create_servo_dev(self, pin: int, min_period=1000, max_period=2000) -> ArduinoServo:
         """
@@ -63,8 +56,6 @@ class ArduinoI2CBus(object):
         write_msg = smbus2.i2c_msg.write(self.arduino_addr, [first_byte_cmd['NEW_SERVO_DEVICE'], pin])
         read_msg = smbus2.i2c_msg.read(self.arduino_addr, 1)
         self.i2c_bus.i2c_rdwr(write_msg, read_msg)
-        device_id = ord(read_msg.buf[0])
-        print('servo device ', device_id)
-        new_servo = ArduinoServo(self.i2c_bus, self.arduino_addr, device_id, min_period=min_period, max_period=max_period)
+        new_servo = ArduinoServo(self.i2c_bus, self.arduino_addr, ord(read_msg.buf[0]), min_period=min_period, max_period=max_period)
         self.devices.append(new_servo)
         return new_servo  # type: ArduinoServo
